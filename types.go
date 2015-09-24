09384e3cb12e9a1deac1c42aef1aa9d563a3f2f3
Lari Rasku <lari.o.rasku@gmail.com>
2015-09-24 08:55:17 +0300

    Define constants for the error types
    Using magic numbers like I do here is bad form, but the alternative
    is to expose (and cast) values from the internal packages in the public
    API, which looks bad.  I think this is better.

--- types.go
+++ types.go
@@ -12,13 +12,20 @@ import (
 	mmuspb "github.com/lxr/go.google.musicmanager/internal/upload_proto/service"
 )
 
-// BUG(lor): The protobuf status enums should probably be exported as
-// individual error variables, rather than types, for ease of
-// comparison.
-
 // A RegisterError is returned by Client.Register if the server refuses
 // to register the client for some reason.
-type RegisterError mmuspb.UploadResponse_AuthStatus
+type RegisterError int32
+
+const (
+	ErrMaxLimitReached RegisterError = 9 + iota
+	ErrClientBoundToOtherAccount
+	ErrClientNotAuthorized
+	ErrMaxPerMachineUsersExceeded
+	ErrClientPleaseRetry
+	ErrNotSubscribed
+	ErrInvalidRequest
+	ErrUpgradeMusicManager
+)
 
 func (e RegisterError) Error() string {
 	return fmt.Sprint("musicmanager register error: ", mmuspb.UploadResponse_AuthStatus(e))
@@ -26,7 +33,14 @@ func (e RegisterError) Error() string {
 
 // A ListError is returned by Client.ListTracks if the server refuses
 // to list the tracks for some reason.
-type ListError mmdspb.GetTracksToExportResponse_TracksToExportStatus
+type ListError int32
+
+const (
+	ErrTransientList ListError = 2 + iota
+	ErrMaxNumClientsReached
+	ErrUnableToAuthenticateClient
+	ErrUnableToRegisterClient
+)
 
 func (e ListError) Error() string {
 	return fmt.Sprint("musicmanager list error: ", mmdspb.GetTracksToExportResponse_TracksToExportStatus(e))
@@ -34,7 +48,20 @@ func (e ListError) Error() string {
 
 // An ImportError is returned by Client.ImportTracks if the server
 // rejects a track based on its metadata or audio sample.
-type ImportError mmuspb.TrackSampleResponse_ResponseCode
+type ImportError int32
+
+const (
+	ErrMatched ImportError = 1 + iota
+	_
+	ErrInvalidSignature
+	ErrAlreadyExists
+	ErrTransientImport
+	ErrPermanentImport
+	ErrTrackCountLimitReached
+	ErrRejectStoreTrack
+	ErrRejectStoreTrackByLabel
+	ErrRejectDrmTrack
+)
 
 func (e ImportError) Error() string {
 	return fmt.Sprint("musicmanager import error: ", mmuspb.TrackSampleResponse_ResponseCode(e))
